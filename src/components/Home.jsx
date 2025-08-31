9edd59e0f45d1a17bf144eb0dfe08277ee990bda
ParthCoder89 <parthgupta5041@gmail.com>
2025-08-31 09:21:45 +0530

    Add some frame motion animations in home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import busImg from "../assets/google map 2.jpg";
 import AOS from "aos";
 import "aos/dist/aos.css";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Home() {
   useEffect(() => {
@@ -15,7 +15,7 @@ export default function Home() {
       className="min-h-screen flex flex-col justify-center items-center pt-20 px-6"
     >
 
-      <h1 className="text-5xl font-bold mb-12 mt-10 text-center"
+      <h1 className="text-5xl font-bold mb-12 mt-4 text-center"
         data-aos="zoom-out"
       >
         Smart Transit System
@@ -51,11 +51,29 @@ export default function Home() {
         <div className="flex justify-center"
           data-aos="zoom-out"
         >
-          <img
-            src={busImg}
-            alt="Tracking"
-            className="rounded-lg shadow-lg w-72"
-          />
+          <motion.div
+            whileHover={{
+              scale: 1.05
+            }}
+            initial={{
+              y: 0
+            }}
+            animate={{
+              x: [0, 50, 50, 0, 0],
+              y: [0, 0, 50, 50, 0]
+            }}
+            transition={{
+              duration: 4,
+              repeat: Infinity
+            }}
+            className="w-72 h-72"
+          >
+            <img
+              src={busImg}
+              alt="Tracking"
+              className="rounded-lg shadow-lg width-[100%]"
+            />
+          </motion.div>
         </div>
       </div>
     </div>
